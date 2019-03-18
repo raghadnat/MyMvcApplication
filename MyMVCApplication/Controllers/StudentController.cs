095e6ab82a828ac611fae61244e0f51e61ee69e3
raghadnat <raghad.natshehh@gmail.com>
2019-03-18 15:30:49 +0200

    first mvc application

---
+++ MyMVCApplication/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using MyMVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        List<Student>  studentList = new List<Student>{
+                            new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                            new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                            new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                            new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                            new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                            new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                            new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+                        };
+
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            
+            return  View(studentList);
+
+           
+        }
+
+        public ActionResult Edit(int id)
+        {
+
+            var st = studentList.Where(x => x.StudentId == id).FirstOrDefault();
+            return View(st);
+
+
+        }
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+
+            
+            return View(std);
+
+
+        }
+
+    }
+}
---
+++ MyMVCApplication/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyMVCApplication.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Required(ErrorMessage = "Please enter student name.")]
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
